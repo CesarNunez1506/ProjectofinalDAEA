5c8c4517ebead835ded9c359de8f9dd75031f5c8
Jhostin565 <yaguno2028@gmail.com>
2025-11-08 10:55:34 -0500

    carpeta

--- Proyecto Final/obj/Debug/net9.0/Proyecto Final.AssemblyInfo.cs
+++ Proyecto Final/obj/Debug/net9.0/Proyecto Final.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto Final")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+704e40fd0f77e94b5cd0b8566ebfc768b756506a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto Final")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto Final")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
