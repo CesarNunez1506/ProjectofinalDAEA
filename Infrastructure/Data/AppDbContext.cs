0c27720e9c49420ac8297c65dc3d7c583a9dfd61
Jhostin565 <yaguno2028@gmail.com>
2025-11-26 19:58:40 -0500

    agregar repositorios de ventas y tiendas

--- Infrastructure/Data/AppDbContext.cs
+++ Infrastructure/Data/AppDbContext.cs
@@ -99,6 +99,9 @@ public partial class AppDbContext : DbContext
     public virtual DbSet<WarehouseResource> WarehouseResources { get; set; }
 
     public virtual DbSet<WarehouseStore> WarehouseStores { get; set; }
+    public  DbSet<Sale> Sales { get; set; }
+    public DbSet<SaleDetail> SaleDetails { get; set; }
+    public DbSet<Store> Stores { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
