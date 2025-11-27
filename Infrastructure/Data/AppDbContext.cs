6672a711dddb33a046a6360e14afd01c241e753c
Jhostin565 <yaguno2028@gmail.com>
2025-11-26 20:11:30 -0500

    agregar repositorios de clientes y métodos de pago

--- Infrastructure/Data/AppDbContext.cs
+++ Infrastructure/Data/AppDbContext.cs
@@ -103,6 +103,12 @@ public partial class AppDbContext : DbContext
     public DbSet<SaleDetail> SaleDetails { get; set; }
     public DbSet<Store> Stores { get; set; }
 
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<PaymentMethod> PaymentMethods { get; set; }
+    public DbSet<CashSession> CashSessions { get; set; }
+    public DbSet<SalesChannel> SalesChannels { get; set; }
+
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=local;Username=admin;Password=admin123");
